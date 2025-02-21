9dbd1fec8faa28a4b2d568f964905fd4ed14b722
Abhaykadam57 <kadamabhay40@gmail.com>
2025-02-21 22:00:17 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React_Vite_CI_CD_Github.git/",
+  base: "/React_Vite_CI_CD_Github/",
   server: {
     open: true,
     port: 3001,
